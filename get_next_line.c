6450c0c505fa4e417a8bf4e9ea0dc6a6554a7771
hbooke <hbooke@student.21-school.ru>
2020-12-29 19:59:25 +0300

    in norme

--- get_next_line.c
+++ get_next_line.c
@@ -15,42 +15,57 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int		get_next_line(int fd, char **line)
+static int	prepare(int fd, char **line, int *need_line)
+{
+	if (fd < 0 || !line)
+		return (-1);
+	*line = malloc(1);
+	**line = 0;
+	*need_line = 1;
+	return (0);
+}
+
+static int	check_long_condition(char **p_endl, t_line_buf *line_buf)
+{
+	return ((*p_endl = ft_memchr(line_buf->buf + line_buf->start, '\n',
+								line_buf->readed - line_buf->start)) &&
+			line_buf->buf + line_buf->readed >= *p_endl &&
+			line_buf->buf + line_buf->start <= *p_endl);
+}
+
+static int	ret_line(char **line, char *p_endl, t_line_buf *line_buf)
+{
+	if (!ft_flush(line, line_buf->buf + line_buf->start,
+				p_endl - line_buf->buf - line_buf->start))
+		return (-1);
+	line_buf->start = (p_endl - line_buf->buf + 1) % BUFFER_SIZE;
+	return (1);
+}
+
+int			get_next_line(int fd, char **line)
 {
 	static t_line_buf	fds[MAX_FILES];
 	char				*p_endl;
 	int					need_line;
 
-	if (fd < 0 || !line)
+	if (prepare(fd, line, &need_line))
 		return (-1);
-	*line = malloc(1);
-	**line = 0;
-	need_line = 1;
 	while (1)
-	{
 		if (!fds[fd].start && need_line)
 		{
 			fds[fd].readed = read(fd, fds[fd].buf, BUFFER_SIZE);
+			need_line = 0;
 			if (fds[fd].readed <= 0)
 				return ((fds[fd].readed != 0) * (-1));
-			need_line = 0;
 		}
-		else if ((p_endl = ft_memchr(fds[fd].buf + fds[fd].start, '\n', fds[fd].readed - fds[fd].start)) &&
-			fds[fd].buf + fds[fd].readed >= p_endl &&
-			fds[fd].buf + fds[fd].start <= p_endl)
-		{
-			if (!ft_flush(line, fds[fd].buf + fds[fd].start,
-				p_endl - fds[fd].buf - fds[fd].start))
-				return (-1);
-			fds[fd].start = (p_endl - fds[fd].buf + 1) % BUFFER_SIZE;
-			return (1);
-		}
+		else if (check_long_condition(&p_endl, &fds[fd]))
+			return (ret_line(line, p_endl, &fds[fd]));
 		else
 		{
-			if (!ft_flush(line, fds[fd].buf + fds[fd].start, fds[fd].readed - fds[fd].start))
+			if (!ft_flush(line, fds[fd].buf + fds[fd].start,
+						fds[fd].readed - fds[fd].start))
 				return (-1);
 			fds[fd].start = 0;
 			need_line = 1;
 		}
-	}
 }
